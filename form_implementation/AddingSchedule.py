697bbb94c0e26926892dde640d433f2dbd5c921c
Miha <>
2023-02-25 16:35:56 +0300

    opening two forms in one window

--- form_implementation/AddingSchedule.py
+++ form_implementation/AddingSchedule.py
@@ -1,22 +1,39 @@
-import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout
 from qtpy import uic
-
-from form_implementation import MireaSchedule
 
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("forms\Form1.ui", self)
-        self.closeButton.clicked.connect(self.close)
-        self.addXMLButton.clicked.connect(self.the_button_was_clicked)
+        self.setLayout(QVBoxLayout())
+        self.add_w1()
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.show()
 
-    def the_button_was_clicked(self):
-        #self.w = MireaSchedule.Widget()
-        uic.loadUi("forms\Form2.ui", self)
-        #self.close()
+    def the_back_button_was_clicked(self):
+        self.del_w(self.layout().itemAt(0).widget())
+        self.add_w1()
+
+    def the_mirea_schedule_button_was_clicked(self):
+        self.add_w2()
+
+    def add_w2(self):
+        w2 = uic.loadUi("forms\Form2.ui")
+        closeButton = w2.findChild(QPushButton, "closeButton")
+        backButton = w2.findChild(QPushButton, "backButton")
+        backButton.clicked.connect(self.the_back_button_was_clicked)
+        closeButton.clicked.connect(self.close)
+        self.layout().replaceWidget(self.layout().itemAt(0).widget(), w2)
+
+    def add_w1(self):
+        w1 = uic.loadUi("forms\Form1.ui")
+        closeButton = w1.findChild(QPushButton, "closeButton")
+        addXMLButton = w1.findChild(QPushButton, "addXMLButton")
+        addXMLButton.clicked.connect(self.the_mirea_schedule_button_was_clicked)
+        closeButton.clicked.connect(self.close)
+        self.layout().addWidget(w1)
+
+    def del_w(self, l):
+        l.deleteLater()
